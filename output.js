de76b4a6144241e7f34c544ed4e2e722ad339431
Gavin Mogan <gavin@gavinmogan.com>
2016-12-22 11:53:29 -0800

    retry on error

--- output.js
+++ output.js
@@ -2,6 +2,9 @@ const promisify = require('es6-promisify-all');
 const funcs = require('./lib/functions.js');
 const fs = promisify(require('fs'));
 const { each, omit, groupBy, keyBy } = require('lodash');
+const errors = require('request-promise/errors');
+
+function sleep(ms = 0) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 function makeOrgData(employees) {
   const topEmployee = employees.find(emp => !emp.supervisor) ||
@@ -17,11 +20,22 @@ function makeOrgData(employees) {
   return groupedBySupervisor[''];
 }
 
+async function getSingleEmployeeData(employee) {
+  try {
+    const newData = await funcs.getEmployeeData(employee.id);
+    return Object.assign(employee, newData);
+  } catch (err) {
+    // handle normal errors
+    if (err instanceof errors.StatusCodeError && err.statusCode === 503) {
+      return sleep(1000).then(() => getSingleEmployeeData(employee));
+    } else {
+      throw err;
+    }
+  }
+}
 async function main() {
   const employees = await funcs.getEmployeesData()
-    .then(employees => Promise.all(employees.map(employee => {
-      return funcs.getEmployeeData(employee.id).then(newData => Object.assign(employee, newData))
-    })));
+    .then(employees => Promise.all(employees.map(getSingleEmployeeData)))
   fs.writeFileSync(`public/employees.json`, JSON.stringify(employees, null, '  '));
   fs.writeFileSync(`public/orgchart.json`, JSON.stringify(makeOrgData(employees), null, '  '));
 
