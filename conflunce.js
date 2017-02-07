2e75963234914578f1f5be6c3708f742cd70602b
Gavin Mogan <gavin@gavinmogan.com>
2017-02-06 22:14:58 -0800

    update all the deps and fix linting

--- conflunce.js
+++ conflunce.js
@@ -1,6 +1,4 @@
-const promisify = require('es6-promisify-all');
 const funcs = require('./lib/functions.js');
-const fs = promisify(require('fs'));
 
 async function main() {
   const page = await funcs.getOrCreatePage('BambooHR Employee Stats', 0);
